538485d00eb9248905c4329cc43ddcef5f9e2d32
guilhermecolares <guilhermegraf17@gmail.com>
2025-02-10 11:46:05 -0300

    CRIEI EX011.js

---
+++ Execícios/AULA12/EX011.js
@@ -0,0 +1,10 @@
+var idade = 11
+if (idade > 12) {
+    console.log('maior que 12 anos')
+} else {
+    if (idade == 12) {
+        console.log('tem 12 anos')
+    } else {
+        console.log ('menor que 12 anos')
+    }
+}
