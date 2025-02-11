08daf6d1f4a3b2c00b0450b8a4c184310fbba925
guilhermecolares <guilhermegraf17@gmail.com>
2025-02-11 13:44:11 -0300

    finalizei o projeto do horario

--- Execícios/AULA13/horario/horario.js
+++ Execícios/AULA13/horario/horario.js
@@ -1,14 +1,13 @@
-var horas = new Date().getHours();
-
 function MudarCorDeFundo() {
+    var horas = 17;
     var cor;
 
     if (horas >= 6 && horas < 12) {
         cor = '#37EDDD';
     } else if (horas >= 12 && horas < 16) {
-        cor = '#F0DB3A';
+        cor = '#CFCF00';
     } else if (horas >= 16 && horas < 19) {
-        cor = '#EDA937';
+        cor = '#CF8C1F';
     } else if (horas >= 19 && horas < 23) {
         cor = '#101218'
     }
@@ -16,13 +15,38 @@ function MudarCorDeFundo() {
     document.body.style.backgroundColor = cor;
 }
 
-MudarCorDeFundo();
-
-setInterval(MudarCorDeFundo, 60000);
-
 function MudarImg() {
+    var horas = 17;
+
     var imgManha = document.getElementById('manha');
-    var imgMeioDia = document.getElementById('meiodia')
-    var imgFimTarde = document.getElementById('fimtarde')
-    var imgNoite = document.getElementById('noite')
+    var imgMeioDia = document.getElementById('meiodia');
+    var imgFimTarde = document.getElementById('fimtarde');
+    var imgNoite = document.getElementById('noite');
+
+    imgManha.classList.remove('mostrar');
+    imgMeioDia.classList.remove('mostrar');
+    imgFimTarde.classList.remove('mostrar');
+    imgNoite.classList.remove('mostrar');
+
+    if (horas >= 6 && horas < 12) {
+        imgManha.classList.add('mostrar')
+    } else if (horas >= 12 && horas < 16) {
+        imgMeioDia.classList.add('mostrar')
+    } else if (horas >= 16 && horas < 18) {
+        imgFimTarde.classList.add('mostrar')
+    } else {
+        imgNoite.classList.add('mostrar')
+    }
+
+    var horario = document.getElementById('horario');
+
+    horario.innerHTML = `Agora são ${horas} Horas.`
 }
+
+MudarCorDeFundo();
+MudarImg();
+
+setInterval(function() {
+    MudarCorDeFundo();
+    MudarImg();
+}, 60000);
