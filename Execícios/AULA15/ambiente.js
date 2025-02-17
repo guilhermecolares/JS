11e65586f5fe1ceab2eb8ca6a8b8a93887be135a
guilhermecolares <guilhermegraf17@gmail.com>
2025-02-17 12:55:31 -0300

    FINALIZAÇÃO DIA 17/02

---
+++ Execícios/AULA15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 4, 3, 7]
+num.push(1)
+num[7] = 6
+console.log(`Nosso ARRAY é o ${num}`)
+console.log(`Nosso ARRAY tem ${num.length} chaves`)
+console.log(`O valor solicitado é o ${num[1]}`)
+console.log(`A Ordem crescente é ${num.sort()}`)
---
+++ Execícios/AULA15/vetornatela.js
@@ -0,0 +1,25 @@
+/*let valores = ['Ferrari', 'Mercedes', 'Lamborghini']
+let linha = ''
+
+for(let chaves = 0; chaves<valores.length; chaves++) {
+    linha += valores[chaves]
+    if (chaves<valores.length - 1) {
+        linha += ', ';
+    }
+}
+
+console.log(`Carros atuais: ${linha}`)
+*/ //FORMATO COM O "FOR" NORMAL
+
+let valores = ['Ferrari', 'Mercedes', 'Lamborghini']
+let linha = ''
+
+for(let chaves in valores) {
+    linha += valores[chaves]
+    if (chaves<valores.length - 1) {
+        linha += ', ';
+    }
+}
+
+console.log(`Carros atuais: ${linha}`) // FORMATO "FOR" PARA ARRAY OU OBJETOS
+console.log(`O índice atual do meu carro é ${valores.indexOf('Mercedes')}`)
